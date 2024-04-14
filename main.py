2ef18cdb8a07cda710b3595beb3ed2ef243d825c
Aleksey-Markov <tte4enuxa@gmail.com>
2024-04-15 00:52:23 +0700

    update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import requests
+import json
 
+import requests
+import psycopg2
 
 params = {'per_page': 100, 'page': 2, 'area': 4}
 
@@ -7,8 +9,29 @@ response = requests.get('https://api.hh.ru/employers/', params=params)
 # print(response.json())
 
 
-# vacs = []
-# for vac in response.json()['items']:
-#     if vac['open_vacancies'] != 0:
-#         vacs.append(vac)
-# print(vacs)
+companies = []
+for company in response.json()['items']:
+    if company['open_vacancies'] != 0:
+        companies.append(company)
+print(companies)
+
+
+conn = psycopg2.connect(
+        host='localhost',
+        database='headhunter',
+        user='postgres',
+        password='83436891'
+)
+cur = conn.cursor()
+# cur.execute('CREATE TABLE company(company_id SERIAL PRIMARY KEY, company_name varchar(100) NOT NULL, company_link text)')
+# cur.execute('CREATE TABLE vacancies(vacancy_id SERIAL PRIMARY KEY, company_id int REFERENCES company(company_id),  vacancy_name varchar(100) NOT NULL, vacancy_salary varchar(25), vacancy_description text)')
+companies_name = []
+for comp in companies:
+    companies_name.append(comp['name'])
+companies_link = []
+for comps in companies:
+    companies_link.append(comps['alternate_url'])
+    cur.execute('INSERT INTO company(company_name) VALUES (%s)', comp["name"])
+conn.commit()
+cur.close()
+conn.close()
