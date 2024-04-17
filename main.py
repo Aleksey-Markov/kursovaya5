fd9c3e9f22efb4a748155eca739ff847725db290
Aleksey-Markov <tte4enuxa@gmail.com>
2024-04-17 23:28:27 +0700

    update

--- main.py
+++ main.py
@@ -15,18 +15,26 @@ def get_vacs(url):
 
 data = get_vacs(url)
 
-companies = []
-for company in data['items']:
-    if company['open_vacancies'] != 0:
-        companies.append(company)
+
+def get_companies_list(file):
+    companies = []
+    with open(file, 'r', encoding='utf=8') as file:
+        f = json.load(file)
+        for company in f:
+            if company['open_vacancies'] != 0:
+                companies.append(company)
+    return companies
 
 
-def save_companies_to_json():
+companies_list = get_companies_list('companies.json')
+
+
+def save_companies_to_json(companies_list):
     with open('companies.json', 'w', encoding='utf=8') as file:
-        json.dump(companies, file, indent=4)
+        json.dump(companies_list, file, indent=4)
 
 
-def generate_db():
+def generate_db(companies_list):
     conn = psycopg2.connect(
             host='localhost',
             database='headhunter',
@@ -34,54 +42,50 @@ def generate_db():
             password=DBPASS
     )
     cur = conn.cursor()
-    # cur.execute('CREATE TABLE company(company_id SERIAL PRIMARY KEY, company_name varchar(25) NOT NULL, company_link text)')
-    # cur.execute('CREATE TABLE vacancies(vacancy_id SERIAL PRIMARY KEY, company_id int REFERENCES company(company_id), vacancy_name varchar(100) NOT NULL, vacancy_salary varchar(25), vacancy_city varchar(25))')
-    # companies_name = []
-    # for comp in companies:
-    #     companies_name.append(comp['name'])
-    # companies_link = []
-    # for comps in companies:
-    #     companies_link.append(comps['alternate_url'])
-    # cur_insert_comps = 'INSERT INTO company(company_name, company_link) VALUES (%s, %s)'
-    # for i in zip(companies_name, companies_link):
-    #     cur.execute(cur_insert_comps, i)
-    # vacancies = []
-    # vacs_name = []
-    # vacs_salary = []
-    # vacs_city = []
-    # with open('companies.json', 'r', encoding='utf=8') as f:
-    #     for vac in json.load(f):
-    #         vacancies.append(vac['vacancies_url'])
-    # num = 0
-    # while num != 11:
-    #     res = requests.get(vacancies[num])
-    #     vac = res.json()['items']
-    #     for names in range(len(vac)):
-    #         vacs_name.append(vac[names]['name'])
-    #     for salarys in range(len(vac)):
-    #         vacs_salary.append(f"{vac[salarys]['salary']['from']} {vac[salarys]['salary']['currency']}")
-    #     for city in range(len(vac)):
-    #         vacs_city.append(vac[city]['area']['name'])
-    #     num += 1
-    # cur_insert_vacs = 'INSERT INTO vacancies(vacancy_name, vacancy_salary, vacancy_city) VALUES (%s, %s, %s)'
-    # for i in zip(vacs_name, vacs_salary, vacs_city):
-    #     cur.execute(cur_insert_vacs, i)
-    # cur_insert_id = 'INSERT INTO vacancies(company_id) VALUES %s'
-    count = []
-    for i in range(len(companies)):
-        count.append(companies[i]['open_vacancies'])
+    cur.execute(
+        '''
+        CREATE TABLE companies(company_id int PRIMARY KEY, company_name varchar(25) NOT NULL, company_link text)
+        '''
+    )
+    for comp in companies_list:
+        company_id = comp['id']
+        company_name = comp['name']
+        company_link = comp['alternate_url']
+        cur.execute('''
+            INSERT INTO companies(company_id, company_name, company_link)
+            VALUES (%s, %s, %s)''', (company_id, company_name, company_link)
+                    )
+    cur.execute(
+        '''
+        CREATE TABLE vacancies(vacancy_id SERIAL PRIMARY KEY, company_id int NOT NULL ,
+         vacancy_name varchar(100) NOT NULL, vacancy_salary varchar(25), vacancy_city varchar(25),
+          FOREIGN KEY (company_id) REFERENCES companies(company_id))
+         '''
+    )
 
-    # cur.execute()
+    num = 0
+    vacancies = []
 
+    with open('companies.json', 'r', encoding='utf=8') as f:
+        for vac in json.load(f):
+            vacancies.append(vac['vacancies_url'])
+    while num != 11:
+        res = requests.get(vacancies[num]).json()['items']
+        print(res)
+        for vac in res:
+            for names, company in zip(range(len((vac))), company_id):
+                company_id = vac['employer']['id']
+                vacs_name = vac['name']
+                vacs_salary = f"{vac['salary']['from']} {vac['salary']['currency']}"
+                vacs_city = vac['area']['name']
+            cur.execute('''
+            INSERT INTO vacancies(company_id, vacancy_name, vacancy_salary, vacancy_city) VALUES (%s, %s, %s, %s)''',
+            (company_id, vacs_name, vacs_salary, vacs_city))
+        num += 1
 
     conn.commit()
     cur.close()
     conn.close()
 
-generate_db()
-# num = 1
-# for i in range(len(companies)):
-#     count = (companies[i]['open_vacancies'])
-#     nums = (str(num) * count)
-#     num += 1
-# print(nums)
+
+generate_db(companies_list)
